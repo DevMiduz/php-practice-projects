fdc13b0db52d3f0c17be55b07fcd93d79505eae6
Dev Miduz <devmiduz@gmail.com>
2025-09-16 14:44:59 +0100

    Add Docker configuration and initial PHP Hello World page

---
+++ 2-docker-hello-world/public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Hello World</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello, World!";
+        ?>
+    </h1>
+    <p>This page is being served by an Apache container with PHP 8.3.</p>
+</body>
+</html>
